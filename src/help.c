1a4c4c7577d888862d39532fa66b7f968825841e
Chirag Khandelwal <ElectruxRedsworth@gmail.com>
2018-06-13 17:56:21 +0530

    Update help.c

--- src/help.c
+++ src/help.c
@@ -14,24 +14,28 @@
 
 #include "../include/help.h"
 
+#define OPT_SPACES 8
+
 void display_help( const char * prog_name )
 {
 	display( "{p}Usage{0}: {s}%s {0}[{t}OPTIONS{0}] {t}LOCATION{0}\n\n", prog_name );
 
 	display( "{p}Options:\n\n" );
 
-	display( "\t{0}-{s}a \t\t{0} - {t}Shows all files{0}/{t}folders{0}, {t}including the hidden ones{0}\n" );
-	display( "\t{0}-{s}A \t\t{0} - {t}Shows all files{0}/{t}folders{0}, {t}including the hidden{0}, {t}but excluding the {0}. {t}and {0}.. {t}ones{0}\n" );
-	display( "\t{0}-{s}d \t\t{0} - {t}Shows only directories{0}\n" );
-	display( "\t{0}-{s}f \t\t{0} - {t}Shows only files{0}\n" );
+	display( "\t{0}--{s}%*s{0} - {t}Show this message and exit\n", OPT_SPACES - 1, "help" );
+	display( "\t{0}--{s}%*s{0} - {t}Display version information and exit\n", OPT_SPACES - 1, "version" );
+
+	display( "\n" );
+
+	display( "\t{0}-{s}%*c{0} - {t}Shows all files{0}/{t}folders{0}, {t}including the hidden ones{0}\n", OPT_SPACES, 'a' );
+	display( "\t{0}-{s}%*c{0} - {t}Shows all files{0}/{t}folders{0}, {t}including the hidden{0}, {t}but excluding the {0}. {t}and {0}.. {t}ones{0}\n", OPT_SPACES, 'A' );
+	display( "\t{0}-{s}%*c{0} - {t}Shows only directories{0}\n", OPT_SPACES, 'd' );
+	display( "\t{0}-{s}%*c{0} - {t}Shows only files{0}\n", OPT_SPACES, 'f' );
 	// TODO:
 	//display( "\t{r}Under construction{0}: -{s}g {0} - {t}Shows files{0}/{t}folders with additional github status{0}, {e}can only be used with the {0}-{s}l {e}option{0}\n" );
-	display( "\t{0}-{s}h \t\t{0} - {t}Shows all sizes in human readable format{0}, {e}can only be used with the {0}-{s}l {e}option{0}\n");
-	display( "\t{0}-{s}i \t\t{0} - {t}Shows inode numbers of all files{0}/{t}folders{0}, {e}can only be used with the {0}-{s}l {e}option{0}\n");
-	display( "\t{0}-{s}l \t\t{0} - {t}Shows all files{0}/{t}directories in a list form with various information about them{0}\n");
+	display( "\t{0}-{s}%*c{0} - {t}Shows all sizes in human readable format{0}, {e}can only be used with the {0}-{s}l {e}option{0}\n", OPT_SPACES, 'h' );
+	display( "\t{0}-{s}%*c{0} - {t}Shows inode numbers of all files{0}/{t}folders{0}, {e}can only be used with the {0}-{s}l {e}option{0}\n", OPT_SPACES, 'i' );
+	display( "\t{0}-{s}%*c{0} - {t}Shows all files{0}/{t}directories in a list form with various information about them{0}\n", OPT_SPACES, 'l' );
 
-	display( "\t{0}-{s}s \t\t{0} - {t}Sorts the output by directories first{0}\n");
-
-	display( "\t{0}--{s}help \t\t{0} - {t}Show this message and exit\n");
-	display( "\t{0}--{s}version \t{0} - {t}Display version information and exit\n" );
+	display( "\t{0}-{s}%*c{0} - {t}Sorts the output by directories first{0}\n", OPT_SPACES, 's' );
 }
